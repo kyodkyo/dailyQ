9d030bfa24161bfbc31723cd8aca18633ec42173
kyodkyo <dkekry@naver.com>
2022-05-28 12:43:12 +0900

    day06

--- src/Q06_1924.java
+++ src/Q06_1924.java
@@ -6,8 +6,18 @@ import java.util.StringTokenizer;
 public class Q06_1924 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
 
+        int[] months = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = new String[]{"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+        int sum = 0;
+        for(int i=0; i<month-1; i++){
+            sum += months[i];
+        }
+        sum += (day-1);
+        System.out.println(days[sum%7]);
     }
 }
---
+++ src/Q07_1427.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class Q07_1427 {
+}
