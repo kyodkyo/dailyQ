8733d86fc768bf606e5e10933c61622ed37e0f10
kyodkyo <dkekry@naver.com>
2022-05-28 12:50:04 +0900

    day07

--- src/Q07_1427.java
+++ src/Q07_1427.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Q07_1427 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q07_1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+
+        Arrays.sort(chars);
+        for (int i = chars.length - 1; i >= 0; i--) {
+            System.out.print(chars[i]);
+        }
+    }
 }
