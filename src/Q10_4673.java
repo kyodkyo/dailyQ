42b5f483b0bba5c57dbf4be92b644720ed46d469
kyodkyo <dkekry@naver.com>
2022-06-01 23:52:24 +0900

    day10

--- src/Q10_4673.java
+++ src/Q10_4673.java
@@ -1,5 +1,27 @@
 public class Q10_4673 {
     public static void main(String[] args) {
-        
+        boolean[] tf = new boolean[10001];
+
+        for(int i=1; i<10001; i++){
+            int n = division(i);
+            if(n<10001){
+                tf[n] = true;
+            }
+        }
+
+        for(int i=1; i<10001; i++){
+            if(!tf[i]){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int division(int n){
+        int sum = n;
+        while (n!=0){
+            sum += (n%10);
+            n = n/10;
+        }
+        return sum;
     }
 }
